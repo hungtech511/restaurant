cb2b0367c41cc2a8573668fc9b801290f825bc2d
hungtech511 <vhung.webdev@gmail.com>
2020-08-25 16:49:07 +0700

    adding my files to my repository

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(window).on('load', function() {
+  $("body").removeClass("preload");
+});
+// Nav
+let openNav = document.querySelector(".open");
+let closeNav = document.querySelector(".close");
+
+  openNav.addEventListener("click",function(){
+    document.getElementById('menu').style.width = '50%';
+  });
+  closeNav.addEventListener("click",function(){
+    document.getElementById('menu').style.width = '0';
+});
+// Carousel
+$('.menu-items').slick({
+  slidesToShow: 3,
+  arrows:true,
+  prevArrow:'.prev',
+  nextArrow:'.next',	  
+  responsive: [
+{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
